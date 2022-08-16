21e1903a02deba019b471fe82f39196605b85a24
Milosz B <miloszbargiel@gmail.com>
2022-08-16 15:56:29 +0200

    Add json validation schemas for db collections

--- src/db-config.ts
+++ src/db-config.ts
@@ -1,17 +1,86 @@
 import * as mongoDB from 'mongodb';
 import dotenv from 'dotenv';
 
-const collections: { posts?: mongoDB.Collection } = {};
+const collections: { posts?: mongoDB.Collection, profiles?: mongoDB.Collection } = {};
 
-const connectToDb = async () => {
+export const connectToDb = async () => {
   dotenv.config();
 
   const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
   await client.connect();
   const db: mongoDB.Db = client.db(process.env.DB_NAME);
-  const postsCollection: mongoDB.Collection = db.collection(process.env.POSTS_COLLECTION_NAME);
 
+  await db.command({
+    "collMod": process.env.POSTS_COLLECTION_NAME,
+    "validator": {
+      $jsonSchema: {
+        bsonType: "object",
+        required: ["username", "avatar", "imageURL", "desc", "likes"],
+        additionalProperties: false,
+        properties: {
+          _id: {},
+          name: {
+            bsonType: "string",
+            description: "'name' is required, type: string",
+          },
+          avatar: {
+            bsonType: "string",
+            description: "'avatar' is required, type: string",
+          },
+          imaegURL: {
+            bsonType: "string",
+            description: "'imaegURL' is required, type: string",
+          },
+          desc: {
+            bsonType: "string",
+            description: "'desc' is required, type: string",
+          },
+          likes: {
+            bsonType: "number",
+            description: "'likes' is required, type: number",
+          },
+        }
+      }
+    }
+  });
+
+  await db.command({
+    "collMod": process.env.PROFILES_COLLECTION_NAME,
+    "validator": {
+      $jsonSchema: {
+        bsonType: "object",
+        required: ["avatar", "username"],
+        additionalProperties: false,
+        properties: {
+          _id: {},
+          username: {
+            bsonType: "string",
+            description: "'username' is required, type: string'",
+          },
+          avatar: {
+            bsonType: "string",
+            description: "'avatar' is required, type: string'",
+          },
+          followers: {
+            bsonType: "number",
+            description: "'followers' is required, type: number'",
+          },
+          following: {
+            bsonType: "number",
+            description: "'following' is required, type: number'",
+          },
+        }
+      }
+    }
+  });
+
+  const postsCollection: mongoDB.Collection = db.collection(process.env.POSTS_COLLECTION_NAME);
+  const profilesCollection: mongoDB.Collection = db.collection(process.env.PROFILES_COLLECTION_NAME);
+
+  collections.profiles = profilesCollection;
   collections.posts = postsCollection;
 
-  console.log(`Connected to ${db.databaseName}, collection: ${postsCollection.collectionName}`);
+  console.log(`Connected to ${db.databaseName}, collections: ${postsCollection.collectionName}, ${profilesCollection.collectionName}`);
 };
+
+connectToDb();

c129494a44785fcee6107d61b0d9c8ccfa9cb8b4
Milosz B <miloszbargiel@gmail.com>
2022-09-05 17:00:42 +0200

    Use mongoose to insert user and post data into the database

--- src/main.ts
+++ src/main.ts
@@ -5,101 +5,84 @@ import {
   PEXELS_RANDOM_IMG_API_URL,
   POST_DESCRIPTION_URL,
   USER_DATA_API_URL,
-  DEFAULT_QUOTES_AMOUNT_TO_FETCH,
-  DEFAULT_USERS_AMOUNT_TO_FETCH,
-  DEFAULT_POSTS_AMOUNT_TO_GENERATE
+  DEFAULT_POSTS_AMOUNT_TO_GENERATE,
 } from './config';
-import { fetchedImageResponse, fetchedPostDescData, fetchedUserData,  usersDataArr, PostData, profileData} from '../types/types';
-import { connectToDb, collections } from './db-config';
-
-const scrapeProfileAndPosts = async () => {
-  dotenv.config();
-  const descriptions: string[] = [];
-  try {
-      await connectToDb();
-      const userData = await fetchUsers()
-      const descriptionsData = await fetchDescriptions();
-      const imagesData = await fetchImages();
-      generatePostsAndProfiles(userData, descriptionsData, imagesData);
-  } catch (e: any) {
-    throw new Error(e.message);
-  }
-};
+import {
+  fetchedImageResponse,
+  fetchedPostDescData,
+  fetchedUserData,
+  filteredUserData,
+} from '../types/types';
+import { connectToDb } from './db-config';
+import User from './models/userModel';
+import Post from './models/postModel';
+import mongoose from 'mongoose';
+dotenv.config();
 
 const convertImage = async (url: string) => {
-  const { data }: {data: ArrayBuffer} = await axios.get(url, { responseType: 'arraybuffer'});
-  const bufferToBase64 = Buffer.from(data).toString('base64');
-  const imageURL = `data:image/jpeg;base64,${bufferToBase64}`;
-  return imageURL;
+  const { data }: { data: ArrayBuffer } = await axios.get(url, { responseType: 'arraybuffer' });
+  return data;
 };
 
-const fetchUsers = async () => {
-  const usersData: usersDataArr = []
-  for (let i = 0; i < DEFAULT_USERS_AMOUNT_TO_FETCH; i++) {
-    const { data }: { data: fetchedUserData } = await axios.get(USER_DATA_API_URL);
-    const desiredUserData = {
-      username: data.results[0].login.username,
-      avatar: await convertImage(data.results[0].picture.large),
-    };
-    usersData.push(desiredUserData)
-  }
-  return usersData;
+const fetchUserData = async () => {
+  const { data }: { data: fetchedUserData } = await axios.get(USER_DATA_API_URL);
+  const desiredUserData: filteredUserData = {
+    username: data.results[0].login.username,
+    email: data.results[0].email,
+    password: data.results[0].login.password,
+    avatar: await convertImage(data.results[0].picture.large),
+  };
+  return desiredUserData;
 };
 
-const fetchDescriptions = async () => {
-  const descriptions: string[] = [];
-  for (let i = 0; i < DEFAULT_QUOTES_AMOUNT_TO_FETCH; i++) {
-    const { data }: {data: fetchedPostDescData} = await axios.get(POST_DESCRIPTION_URL)
-    descriptions.push(data.content);
-  }
-  return descriptions;
+const fetchDesc = async () => {
+  const { data }: { data: fetchedPostDescData } = await axios.get(POST_DESCRIPTION_URL);
+  return data.content;
 };
 
-const fetchImages = async () => {
-  const imageURLs: string[] = [];
-  const { data }: { data: fetchedImageResponse } = await axios.get(PEXELS_RANDOM_IMG_API_URL, {
-    headers: { Authorization: process.env.PEXELS_API_KEY },
-  });
-  const largePhotosURLs = data.photos.map((photo) => photo.src.large);
-  for (const url of largePhotosURLs) {
-    const imageURL = await convertImage(url); 
-    imageURLs.push(imageURL)
-  }
-  return imageURLs;
-};
+const createPostAndUser = async () => {
+  connectToDb();
 
-const generatePostsAndProfiles = (users: usersDataArr, descriptions: string[], images: string[]) => {
-  const posts: PostData[] = [];
-  const profiles: profileData[] = []
-  for (let i = 0; i < DEFAULT_POSTS_AMOUNT_TO_GENERATE; i++) {
-    const { username, avatar } = users[Math.floor(Math.random() * users.length)];
-    const generatedPost = {
+  try {
+    const { username, email, password, avatar } = await fetchUserData();
+    const newUser = new User({
+      _id: new mongoose.Types.ObjectId(),
       username,
+      email,
       avatar,
-      desc: descriptions[i],
-      imageURL: images[i],
-      likes: Math.floor(Math.random() * 500),
+      password,
+      posts: [],
+      following: [],
+      followers: [],
+      profileDesc: 'Some desc',
+    });
+
+    const { data }: { data: fetchedImageResponse } = await axios.get(PEXELS_RANDOM_IMG_API_URL, {
+      headers: { Authorization: process.env.PEXELS_API_KEY },
+    });
+    const largePhotosURLs = data.photos.map((photo) => photo.src.large);
+
+    for (let i = 0; i < DEFAULT_POSTS_AMOUNT_TO_GENERATE; i++) {
+      const photo = Buffer.from(largePhotosURLs[i]);
+      const desc = await fetchDesc();
+
+      const newPost = new Post({
+        desc,
+        photo,
+        _id: new mongoose.Types.ObjectId(),
+        userData: newUser._id,
+        published: new Date(),
+        likedBy: [newUser._id],
+        comments: [],
+      });
+      await newPost.save();
+      newUser.posts.push(newPost._id.toHexString());
     }
-    posts.push(generatedPost);
+    await newUser.save();
+  } catch (e) {
+    console.error(e);
   }
-  for (const user of users) {
-    const { username, avatar } = user;
-    const profileData = {
-      username,
-      avatar,
-      followers: Math.floor(Math.random() * 150),
-      following: Math.floor(Math.random() * 150),
-    }
-    profiles.push(profileData)
-  }
-  sendPostsToDb(posts)
+  await mongoose.connection.close()
 };
 
-const sendPostsToDb = async (posts: PostData[]) => {
-  if (collections.posts) {
-    const result = await collections.posts!.insertMany(posts);
-    console.log(`Added ${posts.length} new posts!`);
-  }
-}
-
-scrapeProfileAndPosts();
+createPostAndUser();
